fd17095a76b62da7568d0f4475f032e7699c680a
jdcardonap2019 <jcardonapaez@gmail.com>
2022-11-10 09:24:56 -0500

    Auth Basic

--- backend/index.js
+++ backend/index.js
@@ -3,12 +3,13 @@ const {auth, resolver, loaders, protocol, circuits} = require('@iden3/js-iden3-a
 const getRawBody = require('raw-body')
 
 const app = express();
-const port = 4200;
+const port = 8080;
 app.use(express.static('static'));
 
 app.get("/api/sign-in", (req, res) => {
     console.log('get Auth Request');
     GetAuthRequest(req,res);
+	console.log('Finished Auth Request')
 });
 
 app.post("/api/callback", (req, res) => {
@@ -17,18 +18,19 @@ app.post("/api/callback", (req, res) => {
 });
 
 app.listen(port, () => {
-    console.log('server running on port 4200');
+    console.log('server running on port 8080');
 });
+
 
 // Create a map to store the auth requests and their session IDs
 const requestMap = new Map();
 
 // GetQR returns auth request
 async function GetAuthRequest(req,res) {
-	console.log('Hola');
+	console.log('Enter in AuthRequestMethod');
 
 	// Audience is verifier id
-	const hostUrl = "localhost:4200";
+	const hostUrl = "https://01dc-186-84-135-86.ngrok.io";
 	const sessionId = 1;
 	const callbackURL = "/api/callback"
 	const audience = "117fsb3hktLw8ie9E6Qj9kfgRHXhLxq9XYaedsJdGU"
@@ -43,13 +45,14 @@ async function GetAuthRequest(req,res) {
 		uri,
 	);
 
-	console.log('Hola2');
+	console.log('Request created');
 	
 	request.id = '7f38a193-0918-4a48-9fac-36adfdb8b542';
 	request.thid = '7f38a193-0918-4a48-9fac-36adfdb8b542';
 
 	// Add request for a specific proof
 	//Age
+	/*
 	const proofRequest  = {
 		id: 1, // request id
 		circuit_id: 'credentialAtomicQuerySig',
@@ -63,8 +66,10 @@ async function GetAuthRequest(req,res) {
 			},
 		},
 		};
+	
+	console.log('ProofRequest created');
 	const scope = request.body.scope ?? [];
-	request.body.scope = [...scope, proofRequest];
+	request.body.scope = [...scope, proofRequest];*/
 
 	//Credit card
 	/*const proofRequest = {
@@ -91,13 +96,16 @@ async function GetAuthRequest(req,res) {
 	
 	// Store auth request in map associated with session ID
 	requestMap.set(`${sessionId}`, request);
-	console.log(requestMap);
+	console.log(res);
+	console.log(request);
 
 	return res.status(200).set('Content-Type', 'application/json').send(request);
 }
 
 // Callback verifies the proof after sign-in callbacks
 async function Callback(req,res) {
+	console.log(req);
+	console.log(res);
 
 	// Get session ID from request
 	const sessionId = req.query.sessionId;
@@ -108,13 +116,20 @@ async function Callback(req,res) {
 
 	// fetch authRequest from sessionID
 	const authRequest = requestMap.get(`${sessionId}`);
+	console.log(requestMap);
+	console.log(authRequest);
 		
 	// Locate the directory that contains circuit's verification keys
 	const verificationKeyloader = new loaders.FSKeyLoader('./keys');
 	const sLoader = new loaders.UniversalSchemaLoader('ipfs.io');
 
+	console.log('Done');
+
 	// Add Polygon Mumbai RPC node endpoint - needed to read on-chain state and identity state contract address
-	const ethStateResolver = new resolver.EthStateResolver('https://polygon-mumbai.g.alchemy.com/v2/NEJA3Eag1uULw6Gdq7b6f2Fe9Nk5G_mE', '0x251dcd7De5f5c8dc0b83F356970F49346692447B');
+	const ethStateResolver = new resolver.EthStateResolver('https://polygon-mumbai.g.alchemy.com/v2/NEJA3Eag1uULw6Gdq7b6f2Fe9Nk5G_mE', '0xb8a86e138C3fe64CbCba9731216B1a638EEc55c8');
+
+	console.log('Done ethStateResolver');
+	console.log(ethStateResolver);
 
 	// EXECUTE VERIFICATION
 	const verifier = new auth.Verifier(
@@ -125,8 +140,11 @@ async function Callback(req,res) {
 
 try {
 	authResponse = await verifier.fullVerify(tokenStr, authRequest);
+	console.log('Done FullVerify');
 } catch (error) {
-return res.status(500).send(error);
-}
+	console.log(error);
+	console.log('Error 500');
+	return res.status(500).send(error);
+	}
 return res.status(200).set('Content-Type', 'application/json').send("user with ID: " + authResponse.from + " Succesfully authenticated");
 }
